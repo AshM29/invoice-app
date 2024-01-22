c14de8ddeabac3c796fd1c2fda4975abef8e5f12
Asmita Mukherjee <85896644+AshM29@users.noreply.github.com>
2024-01-22 20:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_gemini_response(input,image_data,user_prompt):
 def input_image_details(uploaded_file):
   if uploaded_file is not None:
     bytes_data=uploaded_file.getvalue()
-    image_parts=[{'mime_type':uploaded_file.type.'data':bytes_data}]
+    image_parts=[{'mime_type':uploaded_file.type,'data':bytes_data}]
     return image_parts
 
  else:
